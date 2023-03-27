104fef213b7b3933a3fbaaf1162ec437d8b30fd3
Oliver <olichase91@gmail.com>
2023-03-27 13:39:38 -0700

    First Merge

--- src/Components/Home/PeopleInSpace.js
+++ src/Components/Home/PeopleInSpace.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function PeopleInSpace(){
+    const [peopleInSpace, setPeopleInSpace] = useState([])
+
+    useEffect(()=> {
+        fetch('http://api.open-notify.org/astros.json')
+        .then((resp) => resp.json())
+        .then((data) => setPeopleInSpace(data))
+    }, [])
+
+ console.log(peopleInSpace)
 
     return(
         <div>
